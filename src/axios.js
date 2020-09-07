7130062171b4ed008b38d11a1ba9de749f1a1af1
Maisha <maisha.mir2014@gmail.com>
2020-09-07 04:20:05 -0700

    hulu clone files

---
+++ src/axios.js
@@ -0,0 +1,9 @@
+// library used to make request to servers or API endpoint
+
+import axios from 'axios';
+
+const instance = axios.create({
+    baseURL: "https://api.themoviedb.org/3",
+});
+
+export default instance;
